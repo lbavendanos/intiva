fc3c082c662664638406e2ff34312e0f92817541
lbavendanos <lbavendanos@gmail.com>
2026-02-07 20:12:09 -0500

    refactor(auth): simplify login and logout route handlers

--- src/app/(auth)/login/route.ts
+++ src/app/(auth)/login/route.ts
@@ -3,7 +3,3 @@ import { login } from '@/actions/auth'
 export async function GET(): Promise<void> {
   await login()
 }
-
-export async function POST(): Promise<void> {
-  await login()
-}
--- src/app/(auth)/logout/route.ts
+++ src/app/(auth)/logout/route.ts
@@ -1,9 +1,5 @@
 import { logout } from '@/actions/auth'
 
-export async function GET():Promise<void> {
-  await logout()
-}
-
-export async function POST():Promise<void> {
+export async function GET(): Promise<void> {
   await logout()
 }
