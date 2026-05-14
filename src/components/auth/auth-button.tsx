0871aa05f8a7f8022907db4e43e41377904e8240
lbavendanos <lbavendanos@gmail.com>
2026-05-14 09:14:53 -0500

    refactor(auth): switch to User icon

--- src/components/auth/auth-button.tsx
+++ src/components/auth/auth-button.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { LogOut, Package, UserRound } from 'lucide-react'
+import { LogOut, Package, User } from 'lucide-react'
 
 import { __ } from '@/lib/utils'
 import { login, logout } from '@/actions/auth'
@@ -25,7 +25,7 @@ export async function AuthButton() {
           size="icon"
           aria-label={__('auth.login')}
         >
-          <UserRound className="h-5 w-5" />
+          <User className="h-5 w-5" />
         </Button>
       </form>
     )
@@ -35,13 +35,13 @@ export async function AuthButton() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" aria-label={__('auth.account')}>
-          <UserRound className="h-5 w-5" />
+          <User className="h-5 w-5" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem asChild>
           <Link href="/account">
-            <UserRound className="mr-2 h-4 w-4" />
+            <User className="mr-2 h-4 w-4" />
             {__('auth.account')}
           </Link>
         </DropdownMenuItem>
@@ -68,7 +68,7 @@ export async function AuthButton() {
 export function AuthButtonSkeleton() {
   return (
     <Button variant="ghost" size="icon" disabled aria-label={__('auth.login')}>
-      <UserRound className="h-5 w-5" />
+      <User className="h-5 w-5" />
     </Button>
   )
 }
