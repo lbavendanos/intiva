93ecbb5c80c96902a82bc2c485c55ae08e970257
lbavendanos <lbavendanos@gmail.com>
2026-05-15 11:04:48 -0500

    perf(next): optimize @phosphor-icons/react package imports

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   cacheComponents: true,
+  experimental: {
+    optimizePackageImports: ['@phosphor-icons/react'],
+  },
   images: {
     remotePatterns: [
       {
